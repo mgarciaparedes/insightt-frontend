db681edbed9d4e352de344cd5cb9437cb41512b3
mgarciaparedes <46930317+mgarciaparedes@users.noreply.github.com>
2024-06-20 21:09:42 -0400

    feat: navbar

---
+++ src/router/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "../views/Login";
+import Dashboard from "../views/Dashboard";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} /> */}
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/views/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { Navbar } from '../../common/Navbar';
+
+const index = () => {
+  
+  return (
+    <>
+    <Navbar />
+    </>
+  )
+}
+
+export default index
